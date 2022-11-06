ff5c61758145cbd5f33741ad9bcee38b871f538d
Loki Jiang <summer10920@gmail.com>
2022-11-07 00:05:12 +0800

    tableMarker

--- project_Camp/plugins/lokiCalendar.js
+++ project_Camp/plugins/lokiCalendar.js
@@ -1,28 +1,61 @@
-// onload = (() => {
 let
   fetchPath = 'db.json',
-  nationalHoliday = [];
+  nationalHoliday = [],
+  booked = [],
+  pallet = {},
+  calendarCtrl = null,
+  defaultDayType = {
+    date: '',
+    pallet: {
+      aArea: { total: 0, price: 0, count: 0 },
+      bArea: { total: 0, price: 0, count: 0 },
+      cArea: { total: 0, price: 0, count: 0 },
+      dArea: { total: 0, price: 0, count: 0 },
+    }
+  },
+  tableData = {
+    totalPrice: 0,
+    normalCount: 0,
+    holidayCount: 0,
+    nightGo: false,
+    undo: true,
+    pallet: {
+      aArea: { sellCount: 0, sellInfo: '', sumPrice: 0, orderCount: 0 },
+      bArea: { sellCount: 0, sellInfo: '', sumPrice: 0, orderCount: 0 },
+      cArea: { sellCount: 0, sellInfo: '', sumPrice: 0, orderCount: 0 },
+      dArea: { sellCount: 0, sellInfo: '', sumPrice: 0, orderCount: 0 }
+    }
+  };
 
+//dayjs init
 dayjs.locale('zh-tw');
 dayjs.extend(window.dayjs_plugin_localeData);
 dayjs.extend(window.dayjs_plugin_isSameOrBefore);
-
-
+dayjs.extend(window.dayjs_plugin_isBetween);
 
 let init = () => {
   fetch(fetchPath, { method: 'GET' }).then(response => response.json()).then(json => {
-    nationalHoliday = json.nationalHoliday;
+    ({ nationalHoliday, booked, pallet } = json);//語法原為表達式
 
-    showCalendar();
+    calendarCtrl = calendarService();
+    calendarCtrl.print();
+
+    document.querySelector('a[href="#nextCtrl"]').onclick = (event) => {
+      event.preventDefault();
+      calendarCtrl.add();
+    }
+    document.querySelector('a[href="#prevCtrl"]').onclick = (event) => {
+      event.preventDefault();
+      calendarCtrl.sub();
+    }
   })
-
-
-
-
 };
 
-const showCalendar = () => {
-  const today = dayjs();
+const calendarService = () => {
+  const
+    today = dayjs(),
+    chooseDays = [];
+  ;
   let theDay = dayjs(),
     //let theDay = dayjs('2022-12-01');
     objL = {
@@ -45,15 +78,18 @@ const showCalendar = () => {
       }
       for (let i = 1; i <= totalDay; i++) {
         let classStr = 'JsCal';
-        if (theDay.date(i).isSameOrBefore(today)) classStr += ' delDay';
-        else {
-          if (
-            nationalHoliday.includes(obj.thisDate.date(i).format('YYYY-MM-DD')) || (i + firstDay) % 7 < 2
-          ) classStr += ' holiday';
-          classStr += ' emptyDay';
-        }
+        const dateStr = obj.thisDate.date(i).format('YYYY-MM-DD');
 
-        obj.listBox += `<li class="${classStr}">${i}</li>`;
+        if (obj.thisDate.date(i).isSameOrBefore(today)) classStr += ' delDay'; //過期
+        else {
+          const checkDay = booked.find(item => item.date == dateStr);
+          if (checkDay && !(pallet.count - Object.values(checkDay.sellout).reduce((preVal, num) => preVal + num, 0))) //滿帳
+            classStr += ' fullDay';
+          if (nationalHoliday.includes(dateStr) || (i + firstDay) % 7 < 2)
+            classStr += ' holiday';
+          classStr += ' selectDay';
+        }
+        obj.listBox += `<li class="${classStr}" data-date="${dateStr}">${i}</li>`;
       }
       obj.title = `${dayjs.months()[obj.thisDate.month()]} ${obj.thisDate.year()}`;
       return obj;
@@ -63,11 +99,15 @@ const showCalendar = () => {
       document.querySelector('.rightDayList').innerHTML = listMaker(objR).listBox;
       document.querySelector('.leftBar>h4').textContent = objL.title;
       document.querySelector('.rightBar>h4').textContent = objR.title;
+
+      document.querySelectorAll('.selectDay').forEach((item) => {
+        item.onclick = (event) => {
+          calendarCtrl.choose(event);
+        }
+      })
     },
-    changeMonth = (count) => {
-      console.log(theDay);
+    changeMonth = count => {
       theDay = theDay.add(count, 'month');
-      console.log(theDay);
       objL = {
         listBox: '',
         title: '',
@@ -78,25 +118,83 @@ const showCalendar = () => {
         title: '',
         thisDate: theDay.add(1, 'month'),
       };
-
-      listPrint();
-    };
-  listPrint();
-  return {
-    add: function () {
-      changeMonth(1);
     },
-    sub: function () {
+    chooseList = target => {
+      if (!chooseDays[0] && !chooseDays[1]) { // [null,null] => first start
+        chooseDays[0] = target;
+        chooseDays[0].classList.add('selectHead');
+      } else if (chooseDays[0] && !chooseDays[1]) { // [node,null] => first end
+        if (dayjs(target.dataset.date).isSameOrBefore(dayjs(chooseDays[0].dataset.date))) {
+          chooseDays[1] = chooseDays[0];
+          chooseDays[1].classList.replace('selectHead', 'selectFoot');
+          chooseDays[0] = target;
+          chooseDays[0].classList.add('selectHead');
+        } else {
+          chooseDays[1] = target;
+          chooseDays[1].classList.add('selectFoot');
+        }
+
+        //add selectConnect between head and foot
+        document.querySelectorAll('li[data-date]').forEach(item => {
+          if (dayjs(item.dataset.date).isBetween(chooseDays[0].dataset.date, chooseDays[1].dataset.date))
+            item.classList.add('selectConnect');
+        });
+
+        tableMarker();
+
+      } else { //[node,node] => second start
+        //remove connect
+        document.querySelectorAll('li.selectConnect').forEach(
+          item => item.classList.remove('selectConnect')
+        );
+
+        chooseDays[0].classList.remove('selectHead');
+        chooseDays[1].classList.remove('selectFoot');
+        chooseDays[0] = target;
+        chooseDays[0].classList.add('selectHead');
+        chooseDays[1] = null;
+      }
+
+    },
+    tableMarker = () => {
+      for (const key in tableData.pallet)
+        tableData.pallet[key].sellCount = pallet[key].total;
+
+      const aa = document.querySelectorAll('li.selectHead, li.selectConnect').forEach(node => { //one node one day
+        for (const key in tableData.pallet) {
+          const hasOrder = booked.find(item => item.date == node.dataset.date);
+          if (hasOrder)
+            tableData.pallet[key].sellCount = Math.min(tableData.pallet[key].sellCount, pallet[key].total - hasOrder.sellout[key]);
+          if (tableData.pallet[key].sellCount) { //this day can sell
+            const dayPrice = pallet[key][node.classList.contains('holiday') ? 'holidayPrice' : 'normalPrice'];
+            tableData.pallet[key].sumPrice += dayPrice;
+            tableData.pallet[key].sellInfo += `<div>${node.dataset.date}(${dayPrice})</div>`;
+          }
+        }
+      });
+      tablePrint();
+    },
+    tablePrint = () => {
+      const loki=document.querySelectorAll('form select').forEach(node=>{
+        console.log(node);
+      });
+    };
+  return {
+    print: () => listPrint(),
+    add: () => {
+      changeMonth(1);
+      listPrint();
+    },
+    sub: () => {
       changeMonth(-1);
+      listPrint();
+    },
+    choose: e => {
+      const clsList = e.target.classList;
+      if (clsList.contains('selectHead') || clsList.contains('selectFoot')) return;
+      chooseList(e.target);
     }
   }
 }
 
 init();
-
-
-
-
-
-
-// })();
