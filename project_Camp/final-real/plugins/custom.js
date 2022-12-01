e7004a8baf9c4fe6e7135579dfd3fb333b2178cd
Loki Jiang <summer10920@gmail.com>
2022-12-02 00:16:48 +0800

    sleep

--- project_Camp/final-real/plugins/custom.js
+++ project_Camp/final-real/plugins/custom.js
@@ -1,4 +1,6 @@
 onload = () => {
   var grid = document.querySelector('#lokiPark article.row');
   var msnry = new Masonry(grid, { percentPosition: 'true' });
+
+  
 }
--- project_Camp/final/plugins/custom.js
+++ project_Camp/final/plugins/custom.js
@@ -1,4 +1,22 @@
 onload = () => {
   var grid = document.querySelector('#lokiPark article.row');
-  var msnry = new Masonry(grid, { percentPosition: 'true' });
+  new Masonry(grid, { percentPosition: 'true' });
+
+  //menu background effect
+  const menuEffect = () => {
+    const headerMenu = document.querySelector('nav.navbar');
+    let desktopMode = getComputedStyle(headerMenu.querySelector('button')).getPropertyValue('display');
+
+    if (scrollY > 500 || desktopMode != 'none') headerMenu.classList.remove('init');
+    else headerMenu.classList.add('init');
+  }
+
+  onresize = () => {
+    menuEffect();
+  }
+  onscroll = () => {
+    menuEffect();
+  }
+
+  AOS.init();
 }
